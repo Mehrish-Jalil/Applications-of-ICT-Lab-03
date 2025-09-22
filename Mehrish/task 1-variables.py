5a45e0d64131cde2e6fedcfc5da9ab70265e1fbd
Mehrish-Jalil <mehrishjalil2208@gmail.com>
2025-09-22 22:07:18 +0500

    Add files via upload

---
+++ Mehrish/task 1-variables.py
@@ -0,0 +1,8 @@
+name = "Mehrish"
+age = 18
+height = 5.3
+is_student = True
+print(name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
---
+++ Mehrish/task 2-arithmetic.py
@@ -0,0 +1,9 @@
+a = 13
+b = 17
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division (float):", a / b)
+print("Floor Division (integer result):", a // b)
+print("Modulus (remainder):", a % b)
+print("Exponent (power):", a ** b)
